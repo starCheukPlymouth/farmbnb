fc60094933c23c071618b97a5ad6cef6f201dafd
Star Cheuk <183@tranthinktech.com>
2021-12-23 21:15:28 +0800

    Add sign up activity

--- app/src/main/java/com/starcomp/farmbnb/MainActivity.java
+++ app/src/main/java/com/starcomp/farmbnb/MainActivity.java
@@ -2,6 +2,7 @@ package com.starcomp.farmbnb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,23 +11,35 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Activity self;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        self = this;
+
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                System.out.println("Click");
-                goNextPage();
+                Intent intent = new Intent(self, LoginActivity.class);
+                goNextPage(intent);
+            }
+        });
+
+        Button registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(self, RegisterActivity.class);
+                goNextPage(intent);
             }
         });
     }
 
-    private void goNextPage() {
-        Intent intent = new Intent(this, LoginActivity.class);
+    private void goNextPage(Intent intent) {
         startActivity(intent);
     }
 }
