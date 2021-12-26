f93a28ccb8a4e99132164d258f29d95da50cbcec
Star Cheuk <183@tranthinktech.com>
2021-12-26 10:42:02 +0800

    Add validation and error message in register page

--- app/src/main/java/com/starcomp/farmbnb/RegisterActivity.java
+++ app/src/main/java/com/starcomp/farmbnb/RegisterActivity.java
@@ -2,10 +2,12 @@ package com.starcomp.farmbnb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
@@ -13,14 +15,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private LinearLayout linearLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        linearLayout = findViewById(R.id.linearLayout);
 
         ImageButton backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -44,14 +42,39 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void register() {
-        Snackbar snackbar = Snackbar.make(linearLayout, getString(R.string.login_success), Snackbar.LENGTH_INDEFINITE);
-        snackbar.setAction(getString(R.string.home), new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                nextPage();
-            }
-        });
-        snackbar.show();
+        EditText phoneNumberInput = findViewById(R.id.phoneNumberInput);
+        EditText passwordInput = findViewById(R.id.passwordInput);
+        EditText confirmInput = findViewById(R.id.confirmInput);
+
+        String phoneNumber = phoneNumberInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String confirm = confirmInput.getText().toString();
+
+        if (phoneNumber.length() <= 0 || password.length() <= 0 || confirm.length() <= 0) {
+            registerError("Input is empty!");
+            return;
+        }
+
+        if (phoneNumber.length() < 6 || password.length() < 6 || confirm.length() < 6) {
+            registerError("Input is too short!");
+            return;
+        }
+
+        if(!password.equals(confirm)) {
+            registerError("Password is not match!");
+            return;
+        }
+
+        nextPage();
+    }
+
+    private void registerError(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Register Error!")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
     private void nextPage() {
