1aa01f54cc8b06c029128eb5d858107aa0df112a
Star Cheuk <183@tranthinktech.com>
2021-12-26 11:07:30 +0800

    Add notification in success page

--- app/src/main/java/com/starcomp/farmbnb/SuccessActivity.java
+++ app/src/main/java/com/starcomp/farmbnb/SuccessActivity.java
@@ -1,14 +1,22 @@
 package com.starcomp.farmbnb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SuccessActivity extends AppCompatActivity {
+
+    public static String CHANNEL_ID = "Channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,9 @@ public class SuccessActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(ProductActivity.EXTRA_MESSAGE);
         message += " successes booking!\nPlease check your email verification for payment.";
+
+        createNotificationChannel();
+        setNotification(message);
 
         TextView desc = findViewById(R.id.desc);
         desc.setText(message);
@@ -30,6 +41,32 @@ public class SuccessActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void setNotification(String message) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.alert_light_frame)
+                .setContentTitle("FarmBnb booking successful!")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(100, builder.build());
     }
 
     private void back() {
